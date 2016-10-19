b1bf8422b275a3042b13a29eaeb8224cf5472491
davidblancolopez <daviddbl94@gmail.com>
2016-10-19 19:20:53 +0200

    Cambios

--- src/activitat1/pkg3_m9_uf2/Activitat13_M9_UF2.java
+++ src/activitat1/pkg3_m9_uf2/Activitat13_M9_UF2.java
@@ -6,13 +6,14 @@ import java.util.List;
 
 
 public class Activitat13_M9_UF2 {
-    Fils fils = new Fils();
+    
 
     public static void main(String[] args) {
         List<Fils> llistaResultatsMultiples2i3 = new ArrayList<Fils>();
         
-        for (int i = 0; i < 100; i++) {
-            
+        for (int i = 1; i <= 100; i++) {
+            Fils fil = new Fils(int i);
+            llistaResultatsMultiples2i3.add(fil);
         }
         
         
