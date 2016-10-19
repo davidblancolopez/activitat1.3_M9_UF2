96c930b24d1a95e9c2f57f31df2202c6e3993d40
davidblancolopez <daviddbl94@gmail.com>
2016-10-19 17:27:07 +0200

    cambios

--- src/activitat1/pkg3_m9_uf2/Activitat13_M9_UF2.java
+++ src/activitat1/pkg3_m9_uf2/Activitat13_M9_UF2.java
@@ -1,11 +1,16 @@
 
 package activitat1.pkg3_m9_uf2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Activitat13_M9_UF2 {
 
 
     public static void main(String[] args) {
+        List<Multiple> llistaTasques = new ArrayList<Multiple>();
+        
         
     }
     
---
+++ src/activitat1/pkg3_m9_uf2/Multiple.java
@@ -0,0 +1,22 @@
+
+package activitat1.pkg3_m9_uf2;
+
+import java.util.concurrent.Callable;
+
+
+
+
+public class Multiple implements Callable <Integer>{
+    int numComprovar;
+    
+    
+    public Multiple(int numero){
+        this.numComprovar = numero;
+    }
+    
+    
+    public Integer call(){
+        
+        return 0;
+    }
+}
